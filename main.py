846ec4b22b02d8eff2fb4359ad444020ad710f29
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-01-13 18:03:05 -0500

    initial comment

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+take any file type (power point, txt, word, etc and replace whatever words u want, as many times as you like.
+if 3 times, then only first 3, add another input maybe last 3 words. if put -1 then means change all words
+
+'''
+
