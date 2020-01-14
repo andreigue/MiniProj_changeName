1fdb253ab10191ea194489633ad8a5c782fad1ca
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-01-13 21:52:58 -0500

    clean up names

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 '''
-take any file type (power point, txt, word, etc and replace whatever words u want, as many times as you like.
-if 3 times, then only first 3, add another input maybe last 3 words. if put -1 then means change all words
+take any file type (power point, txt, word, etc and replace whatever words u want.
 
 '''
-lines = -1      #reads all lines of file
-words = -1      #replaces all words in file
 
-def wordReplacer(text, wordDict):
+def wordReplacer(original_text, wordDict):
     for key in wordDict:
-        text = text.replace(key, wordDict[key])
-    return text
+        new_Text = original_text.replace(key, wordDict[key])
+    return new_Text
 
-
-f = open("assignment2.txt", "r")
-text = f.read()
 
 wordDic = {
 'original word' : 'new word',
-'Armen': 'Andrei',
+'karl': 'carl',
 }
+
+f = open("file1.txt", "r")
+text = f.read()
 
 newText = wordReplacer(text, wordDic)
 
